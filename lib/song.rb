70de7d764d6009f08305508cab3f6fd9aee8c77a
Natasha Chernis <natashachernis@gmail.com>
2019-04-02 03:56:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,6 +4,7 @@ class Song
 
   @@count = 0
   @@genres = []
+  @@artists = []
 
   def initialize(name, artist, genre)
     @name = name
@@ -11,6 +12,7 @@ class Song
     @genre = genre
     @@count += 1
     @@genres << genre
+    @@artists << artist
   end
 
   def self.count
@@ -19,6 +21,10 @@ class Song
 
   def self.genres
     @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniqu
   end
 
 end
